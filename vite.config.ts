a66a18d664b8acccd2c5323cd7a5abc70ab813ce
SSS444 <santiab222@gmail.com>
2026-05-21 18:15:25 -0500

    Add Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
+  return { base: '/orbit-nft/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
